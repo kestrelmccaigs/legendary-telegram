338bb3c84aa390134d390d7e3d5d34bb77e27829
kestrelmccaigs <rmacagy@gmail.com>
2016-06-13 15:32:40 -0700

    Create control.js

---
+++ js/control.js
@@ -0,0 +1,8 @@
+// Liason between model and view js. Conditional statments, loops, other constructs that execute different decisions based on user input.
+
+console.log("control is linked");
+
+if(0===0){
+	var TestOneTwoThree = TestOneTwo
+	console.log(TestOneTwoThree);
+};
