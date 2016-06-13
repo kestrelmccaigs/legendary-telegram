ea58eea5675b11b94a470eb3b21e5e4278fb0a38
kestrelmccaigs <rmacagy@gmail.com>
2016-06-13 15:33:28 -0700

    Add files via upload

---
+++ js/model.js
@@ -0,0 +1,4 @@
+// Key objects and characters of the game-world.
+
+console.log("model is linked");
+var TestOneTwo = "Testing Testing one two three";
---
+++ js/view.js
@@ -0,0 +1,5 @@
+// Functions that interact directly with the user.
+
+console.log("view is linked");
+document.getElementById("intro").appendChild("<h1>TimeSpace</h1><p>TimeSpace is an interactive choose-your-own-adventure story</p>");
+document.getElementById('story').appendChild(TestOneTwo);
